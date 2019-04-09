54c224b035ae89f18026244e670db2ef2fb2bb24
Glukhikh Vladimir <gluxix1993@gmail.com>
2019-04-09 17:36:46 +0300

    Add InvalidResourceType error

--- lib/multicloud/core/errors.rb
+++ lib/multicloud/core/errors.rb
@@ -3,6 +3,13 @@ module Multicloud
     module Errors
       # Base MultiCloud error
       class Error < StandardError; end
+
+      # Invalid resource type error
+      class InvalidResourceType < Error
+        def initialize
+          super('Invalid resource type')
+        end
+      end
     end
   end
 end
